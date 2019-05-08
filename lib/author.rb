d2fab313200b452d47085d5630e4b5f37b1a69fd
David <david_2_9@hotmail.co.uk>
2019-05-08 14:00:02 +0100

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,15 @@
+class Author
+
+  attr_accessor :name
+
+  def initialize
+    @name = name
+  end
+
+  #  author = Author.new
+  #  postAuthor = Post.author
+  # #
+  #  postAuthor.name = name
+
+
+end
